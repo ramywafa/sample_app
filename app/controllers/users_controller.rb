c2176121564ed70cb346da7ac54b1a0b902593bb
Ramy Wafa <ramy.wafa@gmail.com>
2013-07-02 12:55:04 +0200

    Finished user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+      flash[:success] = "Successfully signed up"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  def index
+    @users = User.all
   end
 end
